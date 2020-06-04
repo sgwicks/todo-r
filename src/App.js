9f65f01228413e0e04b5dbddb0315c65a5c45cd9
sgwicks <samgwicks@gmail.com>
2020-06-04 12:14:33 +0100

    Create Task component, skeleton task list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Task from './components/Task'
 
 function App() {
+  const [taskList] = useState(['Sweep \'til the floor\'s all clean', 'Polish and wax', 'Do laundry', 'Mop and shine up'])
+
   return (
     <div className="App">
-      
+      <h1>Chores</h1>
+      <ul>
+        {taskList.map((task, i) => <Task key={'task' + i} task={task} />)}
+      </ul>
     </div>
   );
 }
---
+++ src/components/Task.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Task = ({task}) => {
+    return (
+    <li>{task}</li>
+    );
+};
+
+export default Task;
