989c056100444be26149fe5a3b307fcf963244ab
sgwicks <samgwicks@gmail.com>
2020-06-04 14:49:50 +0100

    Add new task input text field

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -9,9 +9,16 @@ const List = styled.ul`
 
 const TaskList = ({listName}) => {
     const [taskList, setTaskList] = useState(['Sweep \'til the floor\'s all clean', 'Polish and wax', 'Do laundry', 'Mop and shine up'])
+    const [input, setInput] = useState('Add a new task...')
 
-    const addTask = () => {
-        setTaskList([...taskList, 'Puzzles'])
+    const addTask = (event) => {
+        event.preventDefault();
+        setTaskList([...taskList, input])
+        setInput('')
+    }
+
+    const handleInput = (event) => {
+        setInput(event.target.value)
     }
 
     return (
@@ -19,7 +26,10 @@ const TaskList = ({listName}) => {
         <h2>{listName}</h2>
         <List>
           {taskList.map((task, i) => <Task status='done' key={'task' + i}>{task}</Task>)}
-            <StatusButton onClick={addTask}>[+]</StatusButton>
+        <form onSubmit={addTask}>
+            <StatusButton>[+]</StatusButton>
+            <label><input onClick={() => setInput('')} onChange={handleInput} value={input} /></label>
+        </form>
         </List>
       </>
     );
