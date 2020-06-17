422100a6837d8b3b096e09c6a4e4467f5404d8d7
sgwicks <samgwicks@gmail.com>
2020-06-17 12:47:39 +0100

    create task_lists objectStore if it does not already exist

--- src/App.js
+++ src/App.js
@@ -1,103 +1,118 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList'
 
 const taskList = [
-  { name: "The Usual Morning Line-Up", tasks: [
+  {
+    name: "The Usual Morning Line-Up", tasks: [
       "Sweep 'til the floor's all clean",
       "Polish and wax",
       "Do laundry",
       "Mop and shine-up"
-  ]},
-  { name: "After Lunch", tasks: [
+    ]
+  },
+  {
+    name: "After Lunch", tasks: [
       "Puzzles",
       "Darts",
       "Baking"
-  ]}
+    ]
+  }
 ]
 
 const openDB = (cb) => {
-  const request = window.indexedDB.open('taskListsDB')
-  
+  const request = window.indexedDB.open('taskListsDB', 3)
+
   request.onerror = event => {
     console.log('request error:', event)
   }
-  
+
+  request.onupgradeneeded = event => {
+    const db = event.target.result
+
+    if (!db.objectStoreNames.contains('task_lists')) {
+      db.createObjectStore('task_lists', { autoIncrement: true })
+    } else {
+      console.log('task_lists alreaady exists')
+    }
+  }
+
   request.onsuccess = event => {
     console.log('success!')
     const db = event.target.result;
-  
+
     const clearObjectStore = () => {
       const transaction = db.transaction('task_lists', 'readwrite')
+
       const store = transaction.objectStore('task_lists')
-  
+
 
       const request = store.clear();
 
-    request.onsuccess = event => {
+      request.onsuccess = event => {
         console.log('store cleared')
-    }
+      }
 
-    request.onerror = event => {
+      request.onerror = event => {
         console.error('clearObjectStore', event.target.errorCode)
+      }
+      //   store.openCursor().onsuccess = event => {
+      //     const cursor = event.target.result;
+      //     if (cursor) {
+      //         console.log('cursor.key:', cursor.key);
+      //         const request = store.delete(cursor.key);
+      //         request.onsuccess = event => {
+      //           console.log('cleared ', cursor.key)
+      //         }
+      //         cursor.continue();
+      //     }
+      //     else {
+      //         console.log('No more entries')
+      //     }
+      // }
     }
-    //   store.openCursor().onsuccess = event => {
-    //     const cursor = event.target.result;
-    //     if (cursor) {
-    //         console.log('cursor.key:', cursor.key);
-    //         const request = store.delete(cursor.key);
-    //         request.onsuccess = event => {
-    //           console.log('cleared ', cursor.key)
-    //         }
-    //         cursor.continue();
-    //     }
-    //     else {
-    //         console.log('No more entries')
-    //     }
-    // }
-  }
 
-  clearObjectStore();
+    clearObjectStore();
 
     const addLists = (lists) => {
       const transaction = db.transaction('task_lists', 'readwrite');
-      
+
       transaction.oncomplete = event => {
-          console.log('list added')
+        console.log('list added')
       }
-      
+
       transaction.onerror = event => {
-          console.log('transaction failed:', event)
+        console.log('transaction failed:', event)
       }
-      
+
       const objectStore = transaction.objectStore('task_lists');
-      
+
       lists.forEach(list => {
-          const request = objectStore.add(list);
-          request.onsuccess = event => {
-              console.log('customer added:', event.target.result)
-          }
+        const request = objectStore.add(list);
+        request.onsuccess = event => {
+          console.log('customer added:', event.target.result)
+        }
       })
-  }
+    }
     addLists(taskList)
-  
+
     const getLists = (cb) => {
       const lists = [];
       const store = db.transaction('task_lists').objectStore('task_lists');
-  
+
       store.openCursor().onsuccess = event => {
-          const cursor = event.target.result;
-          if (cursor) {
-              console.log('cursor.key:', cursor.key);
-              const request = store.get(cursor.key);
-              request.onsuccess = event => {
-                lists.push(event.target.result)
-              }
-              cursor.continue();
+        const cursor = event.target.result;
+        if (cursor) {
+          console.log('cursor.key:', cursor.key);
+          const request = store.get(cursor.key);
+          request.onsuccess = event => {
+            lists.push(event.target.result)
           }
-          else {
-              console.log('No more entries')
-              cb(lists)
-          }
+          cursor.continue();
+        }
+        else {
+          console.log('No more entries')
+          cb(lists)
+        }
       }
     }
     getLists(cb)
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,43 +1,42 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import Task from './Task'
 import StatusButton from './StatusButton'
-import taskLists from '../taskLists.json'
 
 const List = styled.ul`
   list-style-type: none;
 `
 
-const TaskList = ({list}) => {
-    const [taskList, setTaskList] = useState([])
-    const [input, setInput] = useState('Add a new task...')
+const TaskList = ({ list }) => {
+  const [taskList, setTaskList] = useState([])
+  const [input, setInput] = useState('Add a new task...')
 
-    useEffect(() => {
-      setTaskList(list.tasks)
-    }, [list])
+  useEffect(() => {
+    setTaskList(list.tasks)
+  }, [list])
 
-    const addTask = (event) => {
-        event.preventDefault();
-        setTaskList([...taskList, input])
-        setInput('')
-    }
+  const addTask = (event) => {
+    event.preventDefault();
+    setTaskList([...taskList, input])
+    setInput('')
+  }
 
-    const handleInput = (event) => {
-        setInput(event.target.value)
-    }
+  const handleInput = (event) => {
+    setInput(event.target.value)
+  }
 
-    return (
-        <>
-        <h2>{list.name}</h2>
-        <List>
-          {taskList.map((task, i) => <Task status='done' key={'task' + i}>{task}</Task>)}
+  return (
+    <>
+      <h2>{list.name}</h2>
+      <List>
+        {taskList.map((task, i) => <Task status='done' key={'task' + i}>{task}</Task>)}
         <form onSubmit={addTask}>
-            <StatusButton>[+]</StatusButton>
-            <label><input onClick={() => setInput('')} onChange={handleInput} value={input} /></label>
+          <StatusButton>[+]</StatusButton>
+          <label><input onClick={() => setInput('')} onChange={handleInput} value={input} /></label>
         </form>
-        </List>
-      </>
-    );
+      </List>
+    </>
+  );
 };
 
 export default TaskList;
